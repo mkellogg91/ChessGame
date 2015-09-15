a21820f4ef6603cf193a438b209cb2f12e7d2a4b
Michael Kellogg <mkellogg91@gmail.com>
2015-09-14 23:16:51 -0400

    beginnings of ChessGame

---
+++ ChessGame/ChessGame/ChessBoard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame
+{
+    public class ChessBoard
+    {
+
+        /*
+
+            Requirement Document:
+
+        Objects:
+            -must have a standard chessboard(graphics)
+            -must have 2 players
+            -players must alternate turns
+            -must have equal amount of chess pieces
+            
+            
+            
+            
+            
+            
+            
+            
+        Actions:
+            -opposing chess pieces must be able to take each other(remove from board)
+            -chess pieces must be able to move correctly
+            -must be able to put king pieces in "check"
+            -must be able to prevent pieces from moving while king in check
+            -must be able to put king in "checkmate"
+
+        */
+
+    }
+}
