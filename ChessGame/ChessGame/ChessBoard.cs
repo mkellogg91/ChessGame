ac036e1caa8d70004520196cd8973853d21c4227
Michael Kellogg <mkellogg91@gmail.com>
2015-09-16 23:06:35 -0400

    beginnings of ChessGame
    The beginnings of the chess game is coming into shape!

--- ChessGame/ChessGame/ChessBoard.cs
+++ ChessGame/ChessGame/ChessBoard.cs
@@ -3,37 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ChessGame
 {
+
+
     public class ChessBoard
     {
+        // PROPERTIES
+        public Panel chessboardPanel { get; set; }
 
-        /*
 
-            Requirement Document:
+        // CONSTRUCTOR HERE
+        public ChessBoard()
+        {
 
-        Objects:
-            -must have a standard chessboard(graphics)
-            -must have 2 players
-            -players must alternate turns
-            -must have equal amount of chess pieces
             
-            
-            
-            
-            
-            
-            
-            
-        Actions:
-            -opposing chess pieces must be able to take each other(remove from board)
-            -chess pieces must be able to move correctly
-            -must be able to put king pieces in "check"
-            -must be able to prevent pieces from moving while king in check
-            -must be able to put king in "checkmate"
 
-        */
+            List<ChessboardSquare> chessboardSquareList = new List<ChessboardSquare>();
+            
+            // CREATING 64 CHESSBOARD SQUARES
+            for (int numOfSquares = 1; numOfSquares < 65; numOfSquares++)
+            {
+                chessboardSquareList[numOfSquares] = new ChessboardSquare();
+
+                // gives each square its appropriate location
+                // give each square its appropriate color
+                // 
+                    
+            }
+
+        } // end constructor
+        
+
 
     }
 }
---
+++ ChessGame/ChessGame/ChessboardSquare.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessGame
+{
+    class ChessboardSquare
+    {
+
+        // POSITION PROPERTIES
+        int squarePos_x { get; set; }
+        int squarePos_y { get; set; }
+        int squareSize_x { get; set; }
+        int squareSize_y { get; set; }
+        // CHESSBOARD SQUARES WILL HAVE A COLOR
+        string squareColor { get; set; }
+        // CHESSBOARD SQUARES WILL HAVE A CHESSPEICE OBJECT
+        ChessPiece squareChessPiece { get; set; }
+
+
+        // ACTUAL VISUAL REPRESENTATION OF CHESSBOARD SQUARE
+        PictureBox squarePictureBox { get; set; }
+
+    }
+}
